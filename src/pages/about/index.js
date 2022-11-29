477ee5e54132e046703202d85933acec903cba5b
GustavoKozlowski <gustavokozlowski96@gmail.com>
2022-11-29 00:46:44 -0300

    page About completa

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React from "react";
+import {
+  ContainerAbout,
+  Img,
+  ALink,
+  Main,
+  Section,
+  Text,
+  Title,
+} from "./styles";
 
 export const About = () => {
   return (
-    <div>Hellow people!!!</div>
-  )
-}
+    <ContainerAbout>
+      <Main>
+        <Section>
+          <Title>O que é o imc?</Title>
+          <Text>
+            Criado no século 19 pelo matemático Lambert Quételet, o Índice de
+            Massa Corporal, conhecido pela sigla IMC, é um cálculo simples que
+            permite medir se alguém está ou não com o peso ideal. Ele aponta se
+            o peso está adequado ou se está abaixo ou acima do peso.
+          </Text>
+          <Text  >
+            O IMC é uma fórmula utilizada por vários profissionais de saúde,
+            incluindo médicos, enfermeiros e nutricionistas, para saber, de uma
+            forma rápida, se a pessoa precisa ganhar ou perder peso.
+          </Text>
+        </Section>
+        <Section>
+          <Img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBOUH-avLbWPDsxyv_HNYAhpeRyF3_BYQzxEc8Qq0IpSEe_6VVCJ_focBdyL15L5tHi-Q&usqp=CAU"
+            alt="legenda das classificações do imc."
+          />
+          <Text marginProps="0vw" fontSize="1rem">
+            fonte:
+            <ALink
+              href="https://brainly.com.br/tarefa/27358316"
+              target="_blank"
+            >
+             https://brainly.com.br/tarefa/27358316
+            </ALink>
+          </Text>
+          <Text marginProps="2vw 0vw" fontSize="1.5rem">
+          <ALink
+              href="https://brainly.com.br/tarefa/27358316"
+              target="_blank"
+            >
+              Clique aqui para ler outro artigo sobre IMC.
+            </ALink>
+          </Text>
+        </Section>
+      </Main>
+    </ContainerAbout>
+  );
+};
---
+++ src/pages/about/styles.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const ContainerAbout = styled.main`
+  padding: 0;
+  margin: 0;
+  background: #fff;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  color: #000;
+`;
+
+export const Main = styled.main`
+  display: flex;
+  background-color: #ddd3ff;
+  flex-direction: column;
+  margin: 5vw;
+  border-radius: 6px;
+`;
+export const Section = styled.section`
+  margin-left: 2vw;
+  paddi
+`;
+export const ALink = styled.a`
+  text-decoration: none;
+  &:hover {
+  color: #6a2a7f;
+},
+    &:active {
+  color: #000;
+}
+`;
+export const Title = styled.h1`
+  font-size: clamp(2.5em, 1em + 1vw, 1.7em);
+  margin: 1vw;
+`;
+export const Img = styled.img`
+    width: 35vw;
+    height: 25vh;
+    border-radius: 6px;
+`;
+
+export const Text = styled.p`
+  
+  font-size: ${(props) => props.fontSize || "clamp(1.5em, 1em + 1vw, 1.7em );"};
+  font-weight: ${(props) => props.fontWeight || "bolder"};
+  text-decoration-line: ${(props) => props.textDecorationLine || "none"};
+  margin: ${(props) => props.marginProps || "1vw"};
+`;
